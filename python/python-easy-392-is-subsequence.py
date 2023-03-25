fce6e27944d3bcc8d16459985acd3bf1b885272b
Treverrio Primandaru <jetskiiiiiii22@gmail.com>
2023-03-25 11:56:12 +0800

    python-easy 392. is subsequence

---
+++ python/python-easy-392-is-subsequence.py
@@ -0,0 +1,29 @@
+"""
+https://leetcode.com/problems/is-subsequence
+
+pseudocode:
+isSubsequence:
+- for every value of s, find its first occurance in t
+- if an occurance is found, t will truncate so that the next search will look from letters that come after
+- also, when an occurance is found, the index will be shifted to represent its index in the original array t rather than the truncated array t
+"""
+
+s, t = "abc", "ahbgdc" # true
+# s, t = "axc", "ahbgdc" # false
+# s, t = "twn", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxtxxxxxxxxxxxxxxxxxxxxwxxxxxxxxxxxxxxxxxxxxxxxxxn" # true
+# s, t = "ab", "baab" # true
+# s, t = "aaaaaa", "bbaaaa" # false
+# s, t = "aaaaaa", "bbabaa" # false
+
+# s, t = "bcd", "uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuubcd" # true
+
+def isSubsequence(s, t):
+    map = [0]
+    try:
+        for x in range(1, len(s)+1):
+            map.append(t[map[-1]:].index(s[x-1])+map[-1]+1)
+    except:
+        return False
+    return True
+
+print(isSubsequence(s, t))
