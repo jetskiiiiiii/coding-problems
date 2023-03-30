3d025a2bff8436c4122f89aba49344d3c8fd5be1
Treverrio Primandaru <jetskiiiiiii22@gmail.com>
2023-03-30 16:33:02 +0800

    python-medium 142. Linked List Cycle II

--- python/leetcode 75/playground.py
+++ python/leetcode 75/playground.py
@@ -1,2 +1,15 @@
-num = 7
-print(int(num / 2) + 1)
+class ListNode:
+    def __init__(self, val=0, next=None) -> None:
+        self.val = val
+        self.next = next
+
+
+list = ListNode(1, ListNode(2, ListNode(0, ListNode(4, ListNode(2)))))
+nodes = [list.val]
+while list:
+    try:
+        if 
+    nodes.append(list.next.val)
+    list = list.next
+
+print(nodes)
