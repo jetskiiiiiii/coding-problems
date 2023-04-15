5a6d7d2ef60cc2a96386935caf19feecaab41195
Treverrio Primandaru <jetskiiiiiii22@gmail.com>
2023-04-16 00:24:40 +0800

    python-easy 206. Reversed Linked List (optimived pt. 2)

--- python/leetcode 75/python-easy-206-reversed-linked-list.py
+++ python/leetcode 75/python-easy-206-reversed-linked-list.py
@@ -2,6 +2,8 @@
 https://leetcode.com/problems/reverse-linked-list/?envType=study-plan&id=level-1
 
 pseudocode:
+
+reverseList
 - if head empty, return head
 - if head not empty, create new list with first value as node with val = head and next = None
 - set head as the next node in linked list
@@ -9,6 +11,9 @@ pseudocode:
 - insert each node at the beginning of the new list, and set next = current node[0]
 - set head as next node in linked list
 - repeat same process until head = None, then return first item of new list
+
+fast/fasterReverseList:
+- create new nodes whose value is head (as we iterate through linked list) and whose next is previous nodes
 """
 
 from typing import Optional
@@ -35,13 +40,31 @@ def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
         return head
 
 
-def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
-    new_node = None
+def fastReverseList(head: Optional[ListNode]) -> Optional[ListNode]:
+    new_head = None
+    # stop when head reaches end node (None)
     while head:
-        new_node = ListNode(head.val, new_node)
+        # create new node where val is current value of head, and next is the previous made node
+        new_head = ListNode(head.val, new_head)
+        # iterate through head
         head = head.next
+    return new_head
 
-    return new_node
+
+def fasterReverseList(head: Optional[ListNode]) -> Optional[ListNode]:
+    new_head = None
+    # stop when head reaches end (None)
+    while head:
+        # hold the next node in list temporariliy
+        temp = head.next
+        # the next attribute of current node is what the reversed list currently is (with new_head representing the reversed list)
+        head.next = new_head
+        # update the reversed list to be the same as current node
+        new_head = head
+        # iterate through the list (as temp == head.next)
+        head = temp
+
+    return new_head
 
 
 print(reverseList(head))
